b59cb54b51373a7c352d6f5e09ccc5537dcb3d80
mititch <mititch@live.com>
2013-11-18 19:22:08 +0200

    compile added

--- src/selector.js
+++ src/selector.js
@@ -4,10 +4,10 @@
 'use strict';
 
 angular.module('ui.selector', [])
-    .directive('uiSelector', [ '$log', '$parse',
-        function (log, $parse) {
+    .directive('uiSelector', [ '$log',
+        function (log) {
 
-            var modalElement = '<div id="myModal" class="modal hide">' +
+            var baseModalElement = angular.element('<div id="myModal" class="modal hide">' +
                 '<div class="modal-body">' +
                 '<div class="radio" ng-repeat="item in data.items">' +
                 '<label>' +
@@ -19,57 +19,64 @@ angular.module('ui.selector', [])
                 '<div class="modal-footer">' +
                 '<button class="btn" ng-click="saveEdit()">Save</button>' +
                 '<button class="btn" ng-click="cancelEdit()">Cancel</button>' +
-                '</div>';
+                '</div>');
 
             return {
                 restrict: 'E',
                 template: '<div class="form-inline">' +
                     '<input type="text" class="form-control" readonly ng-model="selectedItem">' +
                     '<button type="button" class="btn" ng-click="openEdit()">Edit</button>' +
-                    '</div>' +
-                    modalElement,
+                    '</div>',
+                replace: true,
                 scope: {
                     selectedItem: '='
                 },
-                link: function (scope, element, attrs) {
+                compile: function (tElement) {
 
-                    var modalDialog = element.find('.modal');
+                    var modalElement = baseModalElement.clone();
 
-                    scope.data = {
-                        editMode : false,
-                        selectedItemCopy : '',
-                        items : ['First', 'Second', 'Third', 'Fourth', 'Fifth']
-                    };
+                    tElement.append(modalElement);
 
-                    scope.$watch(
-                        function (scope) {
-                            return scope.data.editMode;
-                        },
-                        function (value) {
-                            if (value) {
-                                modalDialog.modal("show");
+                    return function (scope, element) {
+
+                        scope.data = {
+                            editMode: false,
+                            selectedItemCopy: {},
+                            items: ['First', 'Second', 'Third', 'Fourth', 'Fifth']
+
+                        };
+
+                        scope.$watch(
+                            function (scope) {
+                                return scope.data.editMode;
+                            },
+                            function (value) {
+                                if (value) {
+                                    modalElement.modal("show");
+                                }
+                                else {
+                                    modalElement.modal("hide");
+                                }
                             }
-                            else{
-                                modalDialog.modal("hide");
-                            }
+                        );
+
+                        scope.openEdit = function () {
+                            scope.data.selectedItemCopy = scope.selectedItem;
+                            scope.data.editMode = true;
                         }
-                    );
 
-                    scope.openEdit = function () {
-                        scope.data.selectedItemCopy = scope.selectedItem;
-                        scope.data.editMode = true;
+                        scope.saveEdit = function () {
+                            scope.selectedItem = scope.data.selectedItemCopy;
+                            scope.data.editMode = false;
+                        };
+
+                        scope.cancelEdit = function () {
+                            scope.data.editMode = false;
+                        };
                     }
-
-                    scope.saveEdit = function() {
-                        scope.selectedItem = scope.data.selectedItemCopy;
-                        scope.data.editMode = false;
-                    };
-
-                    scope.cancelEdit = function() {
-                        scope.data.editMode = false;
-                    };
                 }
             }
+
         }]
     );
 
